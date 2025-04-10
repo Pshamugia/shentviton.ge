f72bffa69c35b2c79d8e5bfb5757173c3b1b5435
Paata Shamugia <pshamugia@gmail.com>
2025-04-10 15:53:26 +0400

    git custom add text

--- resources/views/products/customize.blade.php
+++ resources/views/products/customize.blade.php
@@ -300,7 +300,19 @@
                     <div class="side-modals" style="padding:5px !important; background-color:#ccc">
 
                         <div class="customization-boxs">
-                             
+                            <div id="textInputsContainer">
+                                {{-- <div class="mb-3">
+                                    <label for="top_text" class="form-label">ზედა ტექსტი</label>
+                                    <input type="text" id="top_text" class="form-control input-styled"
+                                        placeholder="Enter top text">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="bottom_text" class="form-label">ქვედა ტექსტი</label>
+                                    <input type="text" id="bottom_text" class="form-control input-styled"
+                                        placeholder="Enter bottom text">
+                                </div> --}}
+
+                            </div>
                             <button type="button" id="addTextInput" class="btn btn-primary my-2">+ Add Text</button>
                             <div class="mb-3">
                                 <label for="text_color" class="form-label">ტექსტის ფერი</label>
